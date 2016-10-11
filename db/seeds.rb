1a8d111463738d4b165a7481477a84f009885dfc
alex-braun <braunacb@gmail.com>
2016-10-11 16:30:54 -0400

    Add `admin` boolean to user.
    Created an admin user via db/seeds.rb  with admin set to true.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'braunacb@gmail.com', password: 'admin', admin: true)
